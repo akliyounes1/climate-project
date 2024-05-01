fca033d1d492e23823be8f8ffbe4f20cddf73019
AnastassiaFugier <nastia.fugier@gmail.com>
2024-05-01 12:38:11 +0200

    Started js function

--- scriptPage3.js
+++ scriptPage3.js
@@ -1,7 +1,15 @@
 function info1(){
     var country = document.getElementById("country").value;
-    alert(country);
-    if (country=="France"){
-        window.document.
+
+    if(country=="France"){
+        // removing the renewable sources info;
+        var par1 = document.getElementById("par_remove");
+        var img1 = document.getElementById("fig_remove");
+        par1.remove();
+        while(img1.firstChild){
+            img1.firstChild.remove();
+        }
     }
+
+    
 }
